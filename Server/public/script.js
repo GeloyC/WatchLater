33ed006fa63024bcaed45fdd1bd00bf486976145
AngeloC <geloycabangal10@gmail.com>
2025-10-25 00:27:39 +0800

    fetch and save embed YT code to database

--- Server/public/script.js
+++ Server/public/script.js
@@ -1,45 +1,83 @@
+
 let thumbnailList = [];
 
 function createThumb() {
     let link_input = document.querySelector('.link_input').value;
-    let linkEmbed = link_input.replace('watch?v=', 'embed/');
 
+    function parseIframeAttributes(iframe) {
+        const regex = /(\w+)(?:="([^"]*)")?/g;
+        const attrs = {};
 
-    thumbnailList.push({
-        link: linkEmbed,
-    });
+        let match;
 
-    console.log('ThumbList: ', thumbnailList);
+        while((match = regex.exec(iframe))) {
+            const [, key, value ] = match;
 
+            attrs[key] = value !== undefined ? value : true; 
+        }
+
+        return attrs;
+    }
+
+    const clean_embed_link = parseIframeAttributes(link_input);
+
+    // thumbnailList.push({
+    //     link: clean_embed_link.src,
+    //     title:clean_embed_link.title
+    // });
+
+    fetch(`http://localhost:5000/yt_link`, {
+        method: 'POST',
+        headers: {
+            'Content-Type' : 'application/json'
+        }, 
+        body: JSON.stringify({
+            yt_link: clean_embed_link.src,
+            yt_title: clean_embed_link.title
+        })
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            console.error('Error: ', error);
+        });
 }
 
-function clearLinkInput() {
-    document.querySelector('.link_input').value = '';
-}
+
+
 
 
 function displayNewVideo () {
     const thumbnail = document.querySelector('.thumbnail');
     
+    fetch('http://localhost:5000/yt_link_list')
+        .then(response => response.json())
+        .then(data => {
+            thumbnail.innerHTML = '';
+            data.forEach((thumb, index) => {
+                if (thumb.link !== '') {
+                    const linkContainer = document.createElement('a');
+                    linkContainer.classList.add('video_link');
+                    linkContainer.href = thumb.video_link;
+                    linkContainer.target = 'video_container';
 
-    thumbnail.innerHTML = '';
-    thumbnailList.forEach((thumb, index) => {
-        if (thumb.link !== '') {
-            const linkContainer = document.createElement('a');
-            linkContainer.classList.add('video_link');
-            linkContainer.href = thumb.link;
-            linkContainer.target = 'video_container';
-
-            linkContainer.textContent = `Video ${index + 1}`
-            thumbnail.append(linkContainer);
-        } else {
-            console.log('Cant!')
-        }
-
-
+                    linkContainer.textContent = thumb.video_title;
+                    thumbnail.append(linkContainer);
+                } else {
+                    console.log('Cant!')
+                }
+            }
+        )
     })
+    .catch((err) => {
+        console.error('Error fetching data: ', err)
+    }); 
 
 }
+
+displayNewVideo();
 
 
 const btn_addLink = document.querySelector('.button_addLink');
@@ -47,7 +85,28 @@ btn_addLink.addEventListener('click', () => {
     
     createThumb();
     displayNewVideo();
-    clearLinkInput();
+    document.querySelector('.link_input').value = '';
 })
 
+
+
+
+// const YT_embed = '<iframe width="1731" height="656" src="https://www.youtube.com/embed/NiljDyzAOcI" title="The harsh reality of good software" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>';
+
+// function parseIframeAttributes(iframe) {
+//     const regex = /(\w+)(?:="([^"]*)")?/g;
+//     const attrs = {};
+
+//     let match;
+
+//     while((match = regex.exec(iframe))) {
+//         const [, key, value ] = match;
+
+//         attrs[key] = value !== undefined ? value : true; 
+//     }
+
+//     return attrs;
+// }
+
+// const iframeObject = parseIframeAttributes(YT_embed);
 
--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import db from './config/database.js';
 
 
 const app = express();
@@ -10,11 +11,45 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.json());
 
 
 app.get('/' , (req, res) => {
     res.sendFile(path.join(__dirname, '/public', 'index.html'));
 });
+
+
+app.post('/yt_link', async (req, res) => {
+    const { yt_link, yt_title } = req.body;
+
+    try {
+        const [yt_video] = await db.query(
+            `INSERT INTO links (video_link, video_title) VALUES (?, ?)`, 
+            [yt_link, yt_title]);
+
+
+        res.json({
+            message: 'Added link successfully!',
+            insertId: yt_video.insertId 
+        });
+    } catch (err) {
+        console.error('Failed to add video:', err);
+        res.status(500).json({ error: 'Database insert failed' });
+    }
+});
+
+app.get('/yt_link_list', async (req, res) => {
+    try {
+        const [links] = await db.query(
+            `SELECT * FROM links`
+        );
+
+        res.json(links)
+    } catch(err) {  
+        console.error('Failed to fetch links', err );
+    }
+});
+
 
 
 app.listen(PORT, () => {
