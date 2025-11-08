78b6682e326745e4d8e8af13fafd72e187cf9142
AngeloC <geloycabangal10@gmail.com>
2025-11-08 20:36:52 +0800

    fix db connection v3

--- Server/config/database.js
+++ Server/config/database.js
@@ -8,7 +8,7 @@ const db = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
+    database: process.env.DB_NAME || 'x8?zCpmw8Z6R*Uc',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
