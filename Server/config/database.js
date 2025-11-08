72005ec148bc8cad711577be7676e987ff4a9ab0
AngeloC <geloycabangal10@gmail.com>
2025-11-08 20:44:45 +0800

    asdasda

--- Server/config/database.js
+++ Server/config/database.js
@@ -8,9 +8,11 @@ const db = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: 'x8?zCpmw8Z6R*Uc',
+    database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
 });
+
+
 export default db;
