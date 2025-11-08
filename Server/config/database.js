d34b0ba82a7bef3a100042952d20e8912e776651
AngeloC <geloycabangal10@gmail.com>
2025-11-08 20:23:30 +0800

    FIX DB

--- Server/config/database.js
+++ Server/config/database.js
@@ -3,22 +3,6 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-// const db = mysql.createPool({
-//     host: process.env.DB_HOST,
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_NAME,
-//     port: process.env.DB_PORT,
-//     waitForConnections: true,
-//     connectionLimit: 5,
-//     queueLimit: 0,
-// });
-
-
-// console.log('Succesfull connected to database!');
-
-// export default db;
-
 
 let db;
 
